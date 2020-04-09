0cb890ded918ace20db05a66f0e1812a6e2184a6
Siwoon Lim <mongle0629@gmail.com>
2020-04-09 15:15:35 -0700

    New updated

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,10 @@ let userID;
 // performs personalization if a user is logged in.
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-
+        
         // if user is signed in, do...
         let user = firebase.auth().currentUser;
-
+        
         if (user != null) {
             userID = user.uid;
 
@@ -28,13 +28,12 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 // function to personalize the jumbo element.
 // greeting to current user.
-function personalizeJumbo(uID) {
+function personalizeJumbo(uID) { 
     db.collection('users').doc(uID).onSnapshot(
         function (snapshot) {
             document.getElementById('jumbo-h').innerHTML = "Hello " + snapshot.data().name + ",";
             document.getElementById('jumbo-p').innerHTML = "You have completed " + snapshot.data().recipesCompleted + " recipes. <br>";
             document.getElementById('jumbo-btn').innerHTML = "Continue!";
-            document.getElementById('jumbo-btn').href = "recipe.html"; // this goes to a specific recipe.
         }
     );
 }
@@ -42,7 +41,7 @@ function personalizeJumbo(uID) {
 // function to personalize the jumbo element.
 // continue a recipe that is in progress.
 function personalizeRecipe(uID) {
-    db.collection('users').doc(uID).collection('recipesLog').get().then(function (querySnapshot) {
+    db.collection('users').doc(uID).collection('recipesLog').get().then(function (querySnapshot) {                
         let ipRecipeName;
         let ipRecipeProgress;
         let ipRecipeID;
@@ -55,16 +54,17 @@ function personalizeRecipe(uID) {
                 ipRecipeID = doc.id;
             }
         });
-
+       
         // stores recipe id into local storage for later use.
         localStorage.setItem('recipeID', ipRecipeID);
-
+       
         // personalization.
-        if (ipRecipeID == undefined) {
+        if (ipRecipeID == undefined){
             document.getElementById('jumbo-btn').innerHTML = "Start!";
             document.getElementById('jumbo-btn').href = "recipes.html";
         } else {
             document.getElementById('jumbo-p').innerHTML += "You have completed " + ipRecipeProgress + "% of the " + ipRecipeName + " recipe.";
+            document.getElementById('jumbo-btn').href = "recipe-" +ipRecipeName+ ".html"; // this goes to a specific recipe.
         }
     });
 }
@@ -73,10 +73,10 @@ function personalizeRecipe(uID) {
 function saveRecipe(recipeID) {
     let saved = false;
     document.getElementById("save-notice").innerHTML = "";
-
+   
     // check if recipeID exists in users recipesLog
-    db.collection('users').doc(userID).collection('recipesLog').get().then(function (querySnapshot) {
-
+    db.collection('users').doc(userID).collection('recipesLog').get().then(function (querySnapshot) {                
+        
         // loop and check for existing recipeID
         querySnapshot.forEach(function (doc) {
             if (doc.id == recipeID) {
@@ -84,8 +84,8 @@ function saveRecipe(recipeID) {
                 saved = true;
             }
         });
-    }).then(function () {
-        if (!saved) {
+    }).then(function(){
+        if(!saved) {
 
             // go to recipe doc in "recipe" collection
             db.collection('recipes').doc(recipeID).onSnapshot(
@@ -94,8 +94,8 @@ function saveRecipe(recipeID) {
                         .collection('recipesLog').doc(recipeID).set({
                             percentCompleted: 0,
                             recipeName: snapshot.data().name
-                        });
-                    document.getElementById("save-notice").innerHTML = "This recipe has been saved.";
+                    });
+                document.getElementById("save-notice").innerHTML = "This recipe has been saved.";
                 }
             );
         }
